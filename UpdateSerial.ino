f6bd62e491c63fe8642037284daa8680b5daa55a
Parkolay <parkolay@gmail.com>
2021-09-29 09:34:50 -0400

    Update UpdateSerial.ino
    Added more serial messages to debug code

--- UpdateSerial.ino
+++ UpdateSerial.ino
@@ -1,9 +1,28 @@
+/*
+ * UpdateSerial - Added to project for debugging code
+ * this can be removed later if space becomes scarce
+ * 
+ * DAP 09-17-2021 Initial setup
+ * DAP 09-28-2021 Added more serial messages to debug code
+ */
+
 void UpdateSerial()
 {
-        Serial.print("Position1 Value: ");
+        Serial.print("Position 1 Value: ");
         Serial.println(pos1);
         Serial.print("Seconds Value: ");
         Serial.println(seconds);
-//        Serial.print("Position3 Value: ");
+        Serial.print("PixColorRed Value: ");
+        Serial.println(PixColorRed);
+        Serial.print("PixColorGreen Value: ");
+        Serial.println(PixColorGreen);
+        Serial.print("PixColorBlue Value: ");
+        Serial.println(PixColorBlue);
+//        Serial.print("Minutes Value: ");
+//        Serial.println(minutes);
+//        Serial.print("Position 3 Value: ");
 //        Serial.println(pos3);
-}
+//        Serial.print("Seconds Value: ");
+//        Serial.println(hours);
+       
+}// end of void UpdateSerial()
