eb56b16f22c67f8699c739d1f169f6ccc6818414
Parkolay <parkolay@gmail.com>
2021-10-07 14:42:52 -0400

    Update Creepy_Clock.ino
    Added a fourth servo to control the Jaw Bone
    Added a second button to select the servo mapping for the skull assembly at startup
    Added support for 3 more NeoPixels

--- Creepy_Clock.ino
+++ Creepy_Clock.ino
@@ -5,6 +5,9 @@
  *  LOTS of code copied from multiple examples
  *  09-29-2021
  *  
+ *  DAP - 09-17-2021 initial rev
+ *  DAP - 09-28-2021 Added Pixels and  more time varibles
+ *  
 */
 #include <Arduino.h>
 #include <Wire.h>
@@ -17,16 +20,18 @@
 #define NEO_PIN 2
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 2
+#define NUMPIXELS 5
 
-//Add a button as an input
+//Add a buttons for input
 #define BUTTON_1  4
+#define BUTTON_2  5
 
 
 // twelve servo objects can be created on most boards
 Servo myservo1;  // create servo object to control a servo
 Servo myservo2;  // create servo object to control a servo
 Servo myservo3;  // create servo object to control a servo
+Servo myservo4;  // create servo object to control the 'jaw' servo
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
@@ -40,18 +45,26 @@ DS3231 rtc;       //DS3231 rtc support;
 int seconds = 0;  // varible to store the seconds from clock
 int minutes = 0;  // varible to store the minutes from clock
 int hours   = 0;  // varible to store the hours from clock
-int days = 0;  // varible to store the seconds from clock
-int months = 0;  // varible to store the minutes from clock
+int days    = 0;  // varible to store the seconds from clock
+int months  = 0;  // varible to store the minutes from clock
 int years   = 0;  // varible to store the hours from clock
 
-int pos1 = 0;     // variable to store the servo position 1
-int pos2 = 0;     // variable to store the servo position 2
-int pos3 = 0;     // variable to store the servo position 3
+int pos1    = 0;  // variable to store the servo position 1
+int pos2    = 0;  // variable to store the servo position 2
+int pos3    = 0;  // variable to store the servo position 3
+int pos4    = 0;  // variable to store the servo position 3
+int jaw     = 0;  // variable to read the value from the analog pin
 
-int PixelCycles = 6;  //set a number of cycles before turning OFF pixels
-int PixColorRed = 0;  //varible to control the Red portion of the pixel
-int PixColorGreen = 0;//varible to control the Green portion of the pixel
-int PixColorBlue = 0; //varible to control the Blue portion of the pixel
+const int Joystick_JAW = 3;  // analog pin used to connect the 'Jaw' slider  --> "Jaw"
+
+int PosMapButton = 0;
+
+int ButtonState = 0;  // variable to read the value from the joystick button
+
+int PixelCycles   = 6;  //set a number of cycles before turning OFF pixels
+int PixColorRed   = 0;  //varible to control the Red portion of the pixel
+int PixColorGreen = 0;  //varible to control the Green portion of the pixel
+int PixColorBlue  = 0;  //varible to control the Blue portion of the pixel
 
 //this didn't work as well as I had hoped, so it was removed
 //char TimeBuffer [100];  //generate an array for the time display
@@ -60,8 +73,9 @@ int PixColorBlue = 0; //varible to control the Blue portion of the pixel
 void setup() 
 {
   Wire.begin();         //start the I2C, this is also most likely done inside u8g2
-  myservo1.attach(9);   // attaches the servo on pin 9 to the servo object
-  myservo2.attach(10);  // attaches the servo on pin 10 to the servo object
+  myservo1.attach(8);   // attaches the servo on pin 9 to the servo object
+  myservo2.attach(9);  // attaches the servo on pin 10 to the servo object
+  myservo3.attach(10);  // attaches the servo on pin 11 to the servo object
   myservo3.attach(11);  // attaches the servo on pin 11 to the servo object
   Serial.begin(57600);  // open a serial connection
   u8g2.begin();         //start the display
@@ -73,7 +87,11 @@ void setup()
 //rtc.adjust(DateTime(__DATE__, __TIME__)); // force the adjust if the cock is already running
 
   pixels.begin();       // This initializes the NeoPixel library.
+  
   pinMode(BUTTON_1, INPUT_PULLUP);  // initialize the button pin as an input with an internal pull up resistor :
+  pinMode(BUTTON_2, INPUT_PULLUP);  // initialize the button pin as an input with an internal pull up resistor :
+
+  PosMapButton = digitalRead(BUTTON_2);   //read the position Map button input only at startup
   
 }
 
